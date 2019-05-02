4c146a541ac2ebd1d1bfadd784b1045ee8b3ab83
IdoYe <idoye@campus.technion.acl>
2019-05-02 20:20:52 +0300

    debug prefix_sum

--- HW_1/hw1.cu
+++ HW_1/hw1.cu
@@ -71,7 +71,9 @@ long long int distance_sqr_between_image_arrays(uchar *img_arr1, uchar *img_arr2
 __device__ int array_min_positive(int *arr, int len){
     int tid = threadIdx.x;
     __shared__ uchar min_arr[HISTOGRAM_SIZE];
-    min_arr[tid] = arr[tid]; //copy the arr to preserve it
+    if (tid < len) {
+        min_arr[tid] = arr[tid]; //copy the arr to preserve it
+    }
     int half_size = len /2;
     while (half_size >=1){
         if (tid < half_size) {

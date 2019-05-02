37de0e36607b9a0d35a6769980d995c0d476e99a
IdoYe <idoye@campus.technion.acl>
2019-05-02 19:53:43 +0300

    debug arr_min

--- HW_1/hw1.cu
+++ HW_1/hw1.cu
@@ -76,8 +76,8 @@ __device__ int array_min_positive(int *arr, int len){
     int half_size = len /2;
     while (half_size >=1){
         if (tid < half_size) {
-            bool change_flag = (min_arr[tid + half_size] > 0 && min_arr[tid] > min_arr[tid + half_size] ||
-                                min_arr[tid == 0]);
+            bool change_flag = (min_arr[tid + half_size] > 0 && min_arr[tid]
+                    > min_arr[tid + half_size] || min_arr[tid] == 0);
             min_arr[tid] = change_flag * min_arr[tid + half_size] +
                            (!change_flag) * min_arr[tid];
             if(tid == 0) {
